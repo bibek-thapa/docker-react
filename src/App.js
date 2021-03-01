155c44cc800a721480ff6475e12b337dfd050917
Bibek Thapas <bek@MacBook-Pro.local>
2021-03-01 08:07:07 -0600

    Initiol commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Hi there I am bibek. how are you man
         </p>
         <a
           className="App-link"
